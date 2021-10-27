1bc7e15350f93b11d429fc38c94baebe75c37f8a
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-27 10:28:23 +0530

    Add files via upload

---
+++ ReverseNumberUsingRecursion.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReverseNumberUsingRecursion {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the Number : ");
+		int number = in.nextInt();
+	    ReverseNumber(number);
+	       
+
+	}
+	
+	public static void ReverseNumber(int n) {
+		if(n<10) {
+			System.out.print(n);
+		}
+		else {
+			System.out.print(n%10);
+			ReverseNumber(n/10);
+		}
+
+	}
+
+
+}
+//public static int reverse;
+//public static int remainder=0;
